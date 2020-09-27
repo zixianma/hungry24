66faf6d047f3f8e0919b329deaf2598f3509dd44
zixianma <sunny_ma@outlook.com>
2020-09-27 22:22:51 +0800

    modified a little

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -71,11 +71,11 @@ Page({
     })
   },
 
-  toIntroPage() {
-    wx.navigateTo({
-      url: '../intro/intro',
-    })
-  },
+  // toIntroPage() {
+  //   wx.navigateTo({
+  //     url: '../intro/intro',
+  //   })
+  // },
 
   switchTab(e) {
     const tabIndex = e.currentTarget.dataset.tabIndex
@@ -100,7 +100,67 @@ Page({
     wx.navigateTo({
       url: '../donation/donation',
     })
+  },
+  move_horizontal(x, y) {
+    cxt_vertical_line.fillRect(x, y, 3, 414)
+  },
+  move_vertical(x, y) {
+    cxt_horizontal_line.fillRect(x, y, 414, 3)
+  },
+  run: function(){
+    var that = this
+    var cxt = wx.createCanvasContext('game')
+    let rectX_horizontal = 0
+    const rectY_horizontal = 0
+    const rectX_vertical = 0
+    let rectY_vertical = 0
+    let goUp = false
+    let goRight = false
+    // todo add time different 
+
+    setInterval(function() {
+      if (!that.data.isStopped) {
+        if(rectY_vertical == 414) {
+          goUp = true
+        }
+        else if(rectY_vertical == 0) {
+          goUp = false
+        }
+    
+        if(rectX_horizontal == 380) {
+          goRight = true
+        }
+        else if(rectX_horizontal == 0) {
+          goRight = false
+        }
+  
+        cxt.clearRect(0,0,500,700)
+        cxt.setFillStyle('brown')
+  
+        // draw 2 lines
+        goUp ? cxt.fillRect(rectX_vertical,rectY_vertical--,414,3) : cxt.fillRect(rectX_vertical,rectY_vertical++,414,3)
+        cxt.setFillStyle('blue')
+        goRight ? cxt.fillRect(rectX_horizontal--,rectY_horizontal,3,414) : cxt.fillRect(rectX_horizontal++,rectY_horizontal,3,414)
+        
+        // draw 3 points
+        cxt.fillRect(300,70, 10,10)
+        cxt.fillRect(250,300,10,10)
+        cxt.fillRect(126,152,10,10)
+  
+        cxt.draw()
+      }
+    })
+
+
+  },
+  gameControl(e) {
+    const isStopped = this.data.isStopped
+    this.setData({
+      isStopped: !isStopped,
+      gameStatus: isStopped ? "Pause" : "Resume"
+    })
   }
 
 })
+
 
