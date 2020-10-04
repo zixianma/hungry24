bbb12232c391015a668873e128a443e5965990f5
zixianma <sunny_ma@outlook.com>
2020-10-04 18:38:36 +0800

    added the rank page; modified a few elements on the index page

---
+++ miniprogram/pages/rank/rank.js
@@ -0,0 +1,137 @@
+// miniprogram/pages/rank/rank.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        'id': 1,
+        'ranking': 1,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      }, 
+      {
+        'id': 2,
+        'ranking': 2,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 3,
+        'ranking': 3,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 4,
+        'ranking': 4,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 5,
+        'ranking': 5,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 6,
+        'ranking': 6,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 7,
+        'ranking': 7,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 8,
+        'ranking': 8,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 9,
+        'ranking': 9,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      },
+      {
+        'id': 10,
+        'ranking': 10,
+        'userName': 'Sunnie',
+        'userAvatar': "https://hunger24.cfpa.org.cn/images/crop/3.png",
+        'userEnergy': 24
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
